562de87e7da9789397533fcaee144b716ddf7c2f
Lorenzo Gaggini <lgaggini@gmail.com>
2014-03-26 15:07:56 +0100

    micro flavors

--- flavors/micro.py
+++ flavors/micro.py
@@ -3,5 +3,5 @@ flavor = {}
 flavor['sockets'] = '1'
 flavor['cores'] = '1'
 flavor['balloon'] = '0'
-flavor['memory'] = '512'
-flavor['root_size'] = '10G'
+flavor['memory'] = '256'
+flavor['root_size'] = '3G'
